3f6d07153baa33b6a14d90ed576e1d67d0620be5
Lucas José Mendes <llucasmendes@hotmail.com>
2020-08-19 22:55:20 -0300

    chore: home_datasource com funções de salvar e abrir

--- lib/features/home/data/datasource/home_datasouce.dart
+++ lib/features/home/data/datasource/home_datasouce.dart
@@ -1,36 +1,45 @@
+import 'package:meta/meta.dart';
+import 'package:texano/core/data/moor_database.dart';
 import 'package:texano/features/home/domain/entities/lauda.dart';
 
 class HomeDataSource {
+  HomeDataSource({@required this.database});
+
+  final AppDatabase database;
+
   Future<List<Lauda>> getLaudasSalvas() async {
-    // requisição banco de dados
-    return Future<List<Lauda>>.delayed(
-      Duration(milliseconds: 3000),
-      () => [
-        Lauda(
-          id: 1,
-          titulo: 'Reflexões sobre a vida no campo',
-          conteudo: 'x',
-          data: DateTime.now(),
-        ),
-        Lauda(
-          id: 2,
-          titulo: 'O que há além da atmosfera terrestre?',
-          conteudo: 'x',
-          data: DateTime.now(),
-        ),
-        Lauda(
-          id: 3,
-          titulo: 'Não há nada além mar, além mar não há nada',
-          conteudo: 'x',
-          data: DateTime.now(),
-        ),
-        Lauda(
-          id: 4,
-          titulo: 'O passado é a ferida da virtude',
-          conteudo: 'x',
-          data: DateTime.now(),
-        ),
-      ],
+    List<LaudasTableData> laudasBanco = await database.getAllLaudas();
+    return laudasBanco
+        .map(
+          (_) => Lauda(
+            conteudo: _.texto,
+            data: _.data,
+            id: _.id,
+            titulo: _.titulo,
+          ),
+        )
+        .toList();
+  }
+
+  Future insertLauda(Lauda lauda) async {
+    final data = LaudasTableData(
+      id: lauda.id,
+      texto: lauda.conteudo,
+      titulo: lauda.titulo,
+      data: DateTime.now(),
     );
+    database.insertLaudas(data);
+  }
+
+  Future deleteLauda() async {}
+
+  Future updateLauda(Lauda lauda) async {
+    final data = LaudasTableData(
+      id: lauda.id,
+      texto: lauda.conteudo,
+      titulo: lauda.titulo,
+      data: DateTime.now(),
+    );
+    database.updateLaudas(data);
   }
 }
