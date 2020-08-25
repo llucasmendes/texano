872c67c35c4052e9b373d8302dee99c364d7cd03
Lucas José Mendes <llucasmendes@hotmail.com>
2020-08-24 22:44:39 -0300

    feat: modelo de conteudo json

---
+++ lib/features/edicao/data/models/conteudo.dart
@@ -0,0 +1,58 @@
+class Conteudo {
+  List<Segmento> segmento;
+
+  Conteudo({
+    this.segmento,
+  });
+
+  Conteudo.fromJson(Map<String, dynamic> json) {
+    if (json['segmento'] != null) {
+      segmento = new List<Segmento>();
+      json['segmento'].forEach((v) {
+        segmento.add(new Segmento.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.segmento != null) {
+      data['segmento'] = this.segmento.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Segmento {
+  String texto;
+  String cor;
+
+  Segmento({
+    this.texto,
+    this.cor,
+  });
+
+  Segmento.fromJson(Map<String, dynamic> json) {
+    texto = json['texto'];
+    cor = json['cor'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['texto'] = this.texto;
+    data['cor'] = this.cor;
+    return data;
+  }
+}
+
+/**
+ * 
+ {
+    "segmento": [
+        {
+            "texto": "Shyam",
+            "cor": "shyamjaiswal@gmail.com"
+        }
+    ]
+}
+ */
---
+++ lib/features/edicao/presentation/widgets/ritch_editor_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RitchEditorWidget extends StatefulWidget {
+  RitchEditorWidget({Key key}) : super(key: key);
+
+  @override
+  _RitchEditorWidgetState createState() => _RitchEditorWidgetState();
+}
+
+class _RitchEditorWidgetState extends State<RitchEditorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
